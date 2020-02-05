ada9ae74a456cb7f1d4bf89cb8a361d1952f4d8a
iterminal <iterminal>
2020-02-05 16:05:25 +0800

    ?? 加载更多咋整

--- src/components/manga-item/manga-item.tsx
+++ src/components/manga-item/manga-item.tsx
@@ -7,12 +7,12 @@ const MangaItem: Taro.FC<CategoryItem> = ({cover, title, sub_title, authors}) =>
 
   return (
     <View className='mg-manga-item'>
-      <View className='mg-mange-item-cover'>
-        <Image className='mg-mange-item-cover-img' src={cover} />
+      <View className='mg-manga-item-cover'>
+        <Image className='mg-manga-item-cover-img' src={cover} />
       </View>
 
-      <View className='mg-mange-item-title'>{title}</View>
-      <View className='mg-mange-item-subtitle'>{sub_title || authors}</View>
+      <View className='mg-manga-item-title'>{title}</View>
+      <View className='mg-manga-item-subtitle'>{sub_title || authors}</View>
     </View>)
 }
 export default MangaItem
