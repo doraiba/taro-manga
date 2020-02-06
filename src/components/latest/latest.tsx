e14c9eb8c3f493ec0ec629042cbc0cfe3e743fff
iterminal <iterminal>
2020-02-06 19:54:11 +0800

    ?? 类目

--- src/components/latest/latest.tsx
+++ src/components/latest/latest.tsx
@@ -3,7 +3,9 @@ import {LATEST} from "@/contexts/manga-api";
 import ListView from "@/components/list-veiw/list-view";
 import {View} from "@tarojs/components";
 import {MangaItem} from "@/components/manga-item";
-import {AtList} from "taro-ui";
+import {AtIcon, AtList} from "taro-ui";
+
+import './latest.scss'
 
 const Latest: Taro.FC = () => {
 
@@ -12,7 +14,10 @@ const Latest: Taro.FC = () => {
       url={LATEST} renderList={(list = []) => <AtList>
       {list.map((e: LatestItem, i) => {
         return <MangaItem key={i} {...e} >
-          <View>{e.last_update_chapter_name}</View>
+          <View className='mg-latest'>
+            <AtIcon value='tags' size={36} color='#ffbb1a' />
+            <View className='mg-latest-title'>{e.last_update_chapter_name}</View>
+          </View>
         </MangaItem>
       })}
     </AtList>}
