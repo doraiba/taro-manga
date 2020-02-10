e05682ff52dacd2488b05f843614a19b7253c298
iterminal <iterminal>
2020-02-10 18:30:49 +0800

    ?? 提取漫画页头部

---
+++ src/components/manga-header/index.tsx
@@ -0,0 +1,57 @@
+import Taro from '@tarojs/taro'
+import {Block, Image, Text, View} from '@tarojs/components'
+
+import dayjs from "dayjs";
+import SubscribeNow from "@/components/subscribe-now/subscribe-now";
+import StartReading from "@/components/start-reading/start-reading";
+import './index.scss'
+
+const MangaHeader: Taro.FC<Comic & {firstChapter?: number,timestamp?: number}> = ({id,cover, authors, types, hot_num, subscribe_num, status, last_updatetime, firstChapter, timestamp}) => {
+
+  return <Block>
+    <View className='mg-primary-card'>
+      <View className='mg-primary-image-wrapper'>
+        <Image src={cover} className='mg-primary-image' />
+      </View>
+      <View className='mg-primary-header'>
+        <View className='mg-primary-desc'>
+          <View className='mg-primary-authors mg-primary-item'>
+            <View className='at-icon at-icon-user' />
+            <Text selectable className='mg-primary-text'>{authors.map(e => e.tag_name).join()}</Text>
+          </View>
+          <View className='mg-primary-types mg-primary-item'>
+            <View className='at-icon at-icon-bookmark' />
+            <Text selectable className='mg-primary-text'>{types.map(e => e.tag_name).join()}</Text>
+          </View>
+          <View className='mg-primary-hot mg-primary-item'>
+            <View className='at-icon at-icon-bookmark' />
+            <Text selectable className='mg-primary-text'>{`人气 ${hot_num}`}</Text>
+          </View>
+          <View className='mg-primary-subscribe mg-primary-item'>
+            <View className='at-icon at-icon-bookmark' />
+            <Text selectable className='mg-primary-text'>{`订阅 ${subscribe_num}`}</Text>
+          </View>
+          <View className='mg-primary-update mg-primary-item'>
+            <View className='at-icon at-icon-clock' />
+            <Text className='mg-primary-text'>{dayjs.unix(last_updatetime).format('YYYY-MM-DD')} {status.map(e => e.tag_name).join()}</Text>
+          </View>
+        </View>
+        <View className='mg-primary-service'>
+          <SubscribeNow className='mg-primary-button__w80' timestamp={timestamp} oid={id} />
+          <View className='mg-primary-space' />
+          <StartReading className='mg-primary-button__w80 mg-primary-button' timestamp={timestamp} oid={id} cid={firstChapter} />
+        </View>
+      </View>
+    </View>
+  </Block>
+}
+
+MangaHeader.defaultProps = {
+  authors: [],
+  types: [],
+  status: [],
+  chapters: [{data: [{chapter_id: -1}]}],
+  timestamp: dayjs().unix(),
+} as any
+MangaHeader.options = {addGlobalClass: true}
+export default MangaHeader
--- src/pages/manga/manga.tsx
+++ src/pages/manga/manga.tsx
@@ -1,10 +1,9 @@
 import Taro, {useCallback, usePullDownRefresh, useRouter, useState} from '@tarojs/taro'
-import {Block, View, Image,Text} from '@tarojs/components'
+import {Block, View} from '@tarojs/components'
 import {observer} from '@tarojs/mobx';
 import dayjs from "dayjs";
-import StartReading from "@/components/start-reading/start-reading";
-import SubscribeNow from "@/components/subscribe-now/subscribe-now";
 import useComic from "@/hooks/use-comic";
+import MangaHeader from "@/components/manga-header";
 
 import './manga.scss'
 
@@ -20,48 +19,21 @@ const Manga: Taro.FC = () => {
   }, [refetch])
 
   usePullDownRefresh(handleRefresh)
-  const {cover, authors, types, hot_num, subscribe_num, status, last_updatetime, chapters} = data || {
+  const filter = data || {
+    id: parseInt(oid),
     authors: [],
     types: [],
     status: [],
     chapters: [{data: [{chapter_id: -1}]}]
   }
-  const firstChapter = (chapters[0].data.pop() as any).chapter_id
+  const firstChapter = (filter.chapters[0].data.pop() as any).chapter_id
   return (
     <Block>
       <View className='mg-primary'>
-        <View className='mg-primary-card'>
-          <View className='mg-primary-image-wrapper'>
-            <Image src={cover} className='mg-primary-image' />
-          </View>
-          <View className='mg-primary-header'>
-            <View className='mg-primary-desc'>
-              <View className='mg-primary-authors mg-primary-item'>
-                <View className='at-icon at-icon-user' />
-                <Text selectable className='mg-primary-text'>{authors.map(e => e.tag_name).join()}</Text>
-              </View>
-              <View className='mg-primary-types mg-primary-item'>
-                <View className='at-icon at-icon-bookmark' />
-                <Text selectable className='mg-primary-text'>{types.map(e => e.tag_name).join()}</Text>
-              </View>
-              <View className='mg-primary-hot mg-primary-item'>
-                <View className='at-icon at-icon-bookmark' />
-                <Text selectable className='mg-primary-text'>{`人气 ${hot_num}`}</Text>
-              </View>
-              <View className='mg-primary-subscribe mg-primary-item'>
-                <View className='at-icon at-icon-bookmark' />
-                <Text selectable className='mg-primary-text'>{`订阅 ${subscribe_num}`}</Text>
-              </View>
-              <View className='mg-primary-update mg-primary-item'>
-                <View className='at-icon at-icon-clock' />
-                <Text className='mg-primary-text'>{dayjs.unix(last_updatetime).format('YYYY-MM-DD')} {status.map(e => e.tag_name).join()}</Text>
-              </View>
-            </View>
-            <View className='mg-primary-service'>
-              <SubscribeNow className='mg-primary-button__w80' timestamp={timestamp} oid={oid} />
-              <View className='mg-primary-space' />
-              <StartReading className='mg-primary-button__w80 mg-primary-button' timestamp={timestamp} oid={oid} cid={firstChapter} />
-            </View>
+        <MangaHeader {...filter} firstChapter={firstChapter} timestamp={timestamp} />
+        <View className='mg-primary-container'>
+          <View className='mg-primary-toolbar'>
+
           </View>
         </View>
       </View>
