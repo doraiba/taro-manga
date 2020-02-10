70b3b22c34cf43f768b98ab33ac4ae625270fcba
iterminal <iterminal>
2020-02-10 20:50:06 +0800

    ?? 列表item icon添加

--- src/components/manga-item/mange-item/manga-item.tsx
+++ src/components/manga-item/mange-item/manga-item.tsx
@@ -1,16 +1,16 @@
 import Taro from '@tarojs/taro'
-import {Image, View} from '@tarojs/components'
+import {Image, View, Text} from '@tarojs/components'
 import dayjs from "dayjs";
 import {ITouchEvent} from "@tarojs/components/types/common";
 import {MANGA_PAGE} from "@/utils/app-constant";
 
 import './manga-item.scss'
 
-type MangaItemProps = (RankItem & LatestItem) & {onClick?: (event: ITouchEvent,id?: string|number) => any}
+type MangaItemProps = (RankItem & LatestItem) & { onClick?: (event: ITouchEvent, id?: string | number) => any }
 
-const MangaItem: Taro.FC<MangaItemProps> = ({onClick,children, cover, title, authors, types, last_updatetime,id,comic_id}) => {
+const MangaItem: Taro.FC<MangaItemProps> = ({onClick, children, cover, title, authors, types, last_updatetime, id, comic_id}) => {
   return (
-    <View className='mg-manga-union-item' onClick={e => onClick && onClick(e,id || comic_id)}>
+    <View className='mg-manga-union-item' onClick={e => onClick && onClick(e, id || comic_id)}>
       <View className='mg-union-info'>
         <View className='mg-union-image-wrapper'>
           <Image className='mg-union-image' lazyLoad src={cover} />
@@ -18,9 +18,18 @@ const MangaItem: Taro.FC<MangaItemProps> = ({onClick,children, cover, title, aut
         <View className='mg-union-desc'>
           <View className='mg-union-title'>{title}</View>
           <View className='mg-union-produce'>
-            <View className='mg-union-authors'>{authors}</View>
-            <View className='mg-union-tag'>{types}</View>
-            <View className='mg-union-date'>{dayjs.unix((last_updatetime as number) ).format('YYYY-MM-DD HH:mm')}</View>
+            <View className='mg-union-authors'>
+              <View className='at-icon at-icon-user' />
+              <Text selectable>{authors}</Text>
+            </View>
+            <View className='mg-union-tag'>
+              <View className='at-icon at-icon-bookmark' />
+              <Text selectable>{types}</Text>
+            </View>
+            <View className='mg-union-date'>
+              <View className='at-icon at-icon-clock' />
+              <Text>{dayjs.unix((last_updatetime as number)).format('YYYY-MM-DD HH:mm')}</Text>
+            </View>
           </View>
         </View>
       </View>
@@ -31,6 +40,9 @@ const MangaItem: Taro.FC<MangaItemProps> = ({onClick,children, cover, title, aut
 }
 MangaItem.defaultProps = {
   onClick: (_event, id) => Taro.navigateTo({url: `${MANGA_PAGE}?oid=${id}`})
+}
+MangaItem.options = {
+  addGlobalClass: true
 }
 
 export default MangaItem
