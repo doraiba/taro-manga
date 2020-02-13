ddadff9f0061ac29dc4d3dad75e5302831c06560
iterminal <iterminal>
2020-02-13 17:32:26 +0800

    日志

--- src/utils/inject-axios-log.ts
+++ src/utils/inject-axios-log.ts
@@ -4,14 +4,18 @@ const injectDefaultLog = (axios: AxiosInstance,
                           config: Partial<{ request: boolean, response: boolean }> = {request: true}) => {
   axios.interceptors.request.use((value => {
     if(config.request) {
-      console.log("=========开始请求:%o============",value.url)
+      const uniqueId = `${Date.now()}/${(Math.random() * 99).toFixed()}`
+      value['uniqueRequestId'] = uniqueId
+      console.log('===unique id: ',uniqueId,' request config: ',value,'===')
     }
 
     return value
   }));
   axios.interceptors.response.use(value => {
     if(config.response) {
-      console.log("=========开始请求:%o============",value)
+      const uniqueId = value.config['uniqueRequestId']
+      delete value.config['uniqueRequestId']
+      console.log('===unique id: ',uniqueId,' response: ',value,'===')
     }
     return value
   })
